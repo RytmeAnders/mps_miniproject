10170a0b40ec25560431ffe9268b175d19fb34cd
Anders Christiansen <aschristiansen@gmail.com>
2019-08-04 02:53:07 +0200

    Build basic scene, set up camera

--- Assets/scripts/Flight.cs
+++ Assets/scripts/Flight.cs
@@ -6,21 +6,22 @@ public class Flight : MonoBehaviour
 {
 
     Vector3 drag, thrust, gravity, lift, final;
+    Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
+    // No need for "+=" when using rigidbody velocity instead of transform.position
     void Update()
     {
         //get final vector3 combining all 4 forces
         final = Drag() + Thrust() + Gravity() + Lift();
 
-        //TODO add this final vector3 to the plane
-
+        //add this final vector3 to the plane
+        rb.velocity = final;
     }
 
     Vector3 Drag() //Backwards force
@@ -30,6 +31,7 @@ public class Flight : MonoBehaviour
 
     Vector3 Thrust() //Forward force
     {
+        thrust = Vector3.forward; //Testing forward motion
         return thrust;
     }
 
