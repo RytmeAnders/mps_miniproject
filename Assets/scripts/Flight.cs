1387442f9ca7405182c86805bcecddafc27f9a55
Anders Christiansen <aschristiansen@gmail.com>
2019-08-04 14:54:44 +0200

    Gravity working (9.8 m/s^2)

--- Assets/scripts/Flight.cs
+++ Assets/scripts/Flight.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class Flight : MonoBehaviour
 {
+    // Public variables
+    public float gravityCoeff = 9.8f;
+    public float aircraftHeight = 1;
 
+    // Private variables
     Vector3 drag, thrust, gravity, lift, final;
     Rigidbody rb;
 
@@ -17,11 +21,12 @@ public class Flight : MonoBehaviour
     // No need for "+=" when using rigidbody velocity instead of transform.position
     void Update()
     {
-        //get final vector3 combining all 4 forces
-        final = Drag() + Thrust() + Gravity() + Lift();
+        // Get final vector3 combining all 4 forces
+        final = Drag() + Thrust() + CalculateGravity() + Lift();
 
-        //add this final vector3 to the plane
+        // Add this final vector3 to the plane
         rb.velocity = final;
+        print(rb.velocity);
     }
 
     Vector3 Drag() //Backwards force
@@ -35,8 +40,21 @@ public class Flight : MonoBehaviour
         return thrust;
     }
 
-    Vector3 Gravity() //Downwards force
+    // Downwards force
+    /// <summary>
+    /// Subtract gravity coefficient (9.8 m/s^2) every frame if above ground.
+    /// This will accelerate downwards force over time.
+    /// </summary>
+    /// <returns></returns>
+    Vector3 CalculateGravity()
     {
+        if(transform.position.y > aircraftHeight)
+        {
+            gravity.y -= gravityCoeff * Time.deltaTime;
+            return gravity;
+        }
+
+        gravity.y = 0;
         return gravity;
     }
 
